0fccf59fdf70bb2da5b28d9f0e11b71afee92850
claudiop1975 <claudio.parache@gmail.com>
2023-03-03 20:21:07 -0300

    Un par de detalles más a RyM y faltan 990

--- Front/src/components/Error/Error.jsx
+++ Front/src/components/Error/Error.jsx
@@ -1,9 +1,16 @@
-
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import "./Error.css"
+<Error className="css"></Error>
 export default function Error(props) {
+const navigate = useNavigate();
 
-    return (
-       <div>
-         soy Error
+   return (
+      <div className="err">
+       <div className='errin'>
+         <iframe title="Error" src="https://giphy.com/embed/CMBJumYBS30viPWE74" class="giphy-embed" allowFullScreen>         </iframe>
        </div>
-    );
- }
+       <button Style= "width: 120px; height: 50px; font-Size: 20px" onClick={()=>navigate(-1)}>Back</button>
+      </div>
+   );
+}
